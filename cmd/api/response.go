b3cb5f097db1bf36a55f28e32c6b637f5f6298a0
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-25 00:11:50 +0200

    update the method to parse to json the dates

--- cmd/api/response.go
+++ cmd/api/response.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type item struct {
 	Title       string    `json:"title"`
@@ -9,6 +12,23 @@ type item struct {
 	Link        string    `json:"link"`
 	PublishDate time.Time `json:"publish_date"`
 	Description string    `json:"description"`
+}
+
+func (i item) MarshalJSON() ([]byte, error) {
+	var dTime time.Time
+	var dateStr string
+	if i.PublishDate != dTime {
+		dateStr = i.PublishDate.Format("2006-01-02T15:04:05Z")
+	}
+
+	type aItem item
+	return json.Marshal(struct {
+		aItem
+		PublishDate string `json:"publish_date"`
+	}{
+		aItem:       (aItem)(i),
+		PublishDate: dateStr,
+	})
 }
 
 type response struct {
