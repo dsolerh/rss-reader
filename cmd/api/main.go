3e47bbc31804c6ddf8ed7fbcb29fec685ae07d51
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-24 18:01:17 +0200

    create the handler as well as the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	reader "github.com/dsolerh/go-rss-reader"
+)
+
+func feedReader(w http.ResponseWriter, r *http.Request) {
+	urls := r.URL.Query()["urls"]
+
+	items := reader.Parse(urls...)
+
+	resp := response{}
+	resp.Items = make([]item, 0, len(items))
+
+	for _, i := range items {
+		resp.Items = append(resp.Items, item(i))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/", feedReader)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
