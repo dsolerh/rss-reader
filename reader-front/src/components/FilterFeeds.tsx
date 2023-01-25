c832db98dcddb46c85d6cecdfae9f684334f68c4
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-25 18:52:51 +0200

    create the filter component

---
+++ reader-front/src/components/FilterFeeds.tsx
@@ -0,0 +1,57 @@
+import { FormEvent } from "react"
+import useInput from "../hooks/useInput"
+import { RSSFeedFilter } from "../types/RSSFeedFilter"
+import { Input } from "./Input"
+
+
+interface FilterFeedsProps {
+    onFilter: (filter: RSSFeedFilter) => void
+}
+export const FilterFeeds = ({ onFilter }: FilterFeedsProps) => {
+    const title = useInput("", "", () => false)
+    const source = useInput("", "", () => false)
+    const date = useInput("", "", () => false)
+
+    const submitForm = (e: FormEvent) => {
+        e.preventDefault()
+        onFilter({
+            title: title.value,
+            source: source.value,
+            publish_date: date.value,
+        })
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <Input
+                id="title"
+                type="text"
+                label="Title"
+                value={title.value}
+                error={title.error}
+                onBlur={title.onBlurHandler}
+                onChange={title.onChangeHandler}
+            />
+            <Input
+                id="title"
+                type="text"
+                label="Title"
+                value={title.value}
+                error={title.error}
+                onBlur={title.onBlurHandler}
+                onChange={title.onChangeHandler}
+            />
+            <Input
+                id="title"
+                type="text"
+                label="Title"
+                value={title.value}
+                error={title.error}
+                onBlur={title.onBlurHandler}
+                onChange={title.onChangeHandler}
+            />
+            <button type="submit">Filter</button>
+        </form>
+
+    )
+}
