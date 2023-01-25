ab1649a334bc8081318e3cd7f48f05e88d3a758a
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-25 14:02:07 +0200

    create the control panel component

---
+++ reader-front/src/components/ControlPanel.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import useInput from '../hooks/useInput'
+import { FeedSourceItem } from './FeedSourceItem'
+import { Input } from './Input'
+
+interface ControlPanelProps {
+    onChangeFeed: (feeds: string[]) => void
+}
+
+export const ControlPanel = ({ onChangeFeed }: ControlPanelProps) => {
+    const feed = useInput("", "The value must not be empty", (e) => e.trim() === "")
+    const [feeds, setFeeds] = useState<string[]>([])
+
+    const addFeed = () => {
+        setFeeds(feeds => [...feeds, feed.value])
+    }
+
+    const deleteFeed = (index: number) => {
+        setFeeds(feeds => { feeds.splice(index, 1); return [...feeds] })
+    }
+
+    useEffect(() => { onChangeFeed(feeds) }, [feeds, onChangeFeed])
+
+    const feeedList = feeds.map((feed, index) => (
+        <FeedSourceItem
+            key={index}
+            link={feed}
+            index={index}
+            onDelete={deleteFeed}
+        />
+    ))
+
+    return (
+        <div>
+            <ul>{feeedList}</ul>
+            <Input
+                id="feed"
+                label="Fedd Source"
+                type="text"
+                value={feed.value}
+                error={feed.error}
+                onChange={feed.onChangeHandler}
+                onBlur={feed.onBlurHandler}
+            />
+            <button onClick={addFeed}>Add Source</button>
+        </div>
+    )
+}
