89eb3cd108d7f40c4cfd78c138fe53d484bb743d
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-25 14:02:20 +0200

    create the feed source item

---
+++ reader-front/src/components/FeedSourceItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface FeedSourceItemProps {
+    index: number
+    link: string
+    onDelete: (index: number) => void
+}
+
+export const FeedSourceItem = ({ link, index, onDelete }: FeedSourceItemProps) => {
+    return (
+        <li key={index}>
+            {link}
+            <button onClick={() => { onDelete(index) }}></button>
+        </li>
+    )
+}
