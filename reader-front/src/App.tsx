26e12cc8e01f273d9cb566b6efc8f96621011ec6
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-25 14:02:29 +0200

    update the app

--- reader-front/src/App.tsx
+++ reader-front/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { ControlPanel } from './components/ControlPanel';
 
 function App() {
     return (
         <div>
+            <ControlPanel onChangeFeed={(feeds) => { console.log(feeds) }} />
         </div>
     );
 }
