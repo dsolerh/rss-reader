c8e4547f80f19fe7683acadcbc1a9dae71c000bf
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-26 16:35:22 +0200

    style the component and add the logic for the loading state and for the 'no data' state

--- reader-front/src/components/MainPanel.tsx
+++ reader-front/src/components/MainPanel.tsx
@@ -3,9 +3,11 @@ import { RSSFeedFilter } from "../types/RSSFeedFilter"
 import { RSSItem } from "../types/RSSItem"
 import { FilterFeeds } from "./FilterFeeds"
 import { RSSFeedItem } from "./RRSFeedItem"
+import { RSSFeedPlaceholder } from "./RSSFeedPlaceholder"
 
 interface MainPanelProps {
     feeds: RSSItem[]
+    loading: boolean
     onFilter: (filter: RSSFeedFilter) => void
 }
 
@@ -13,18 +15,34 @@ function toMili(s: string) {
     return new Date(s).valueOf()
 }
 
-export const MainPanel = ({ feeds, onFilter }: MainPanelProps) => {
+export const MainPanel = ({ feeds, onFilter, loading }: MainPanelProps) => {
     const [showFilter, setShowFilter] = useState(false)
-    const feedsList = feeds
-        .sort((a, b) => toMili(a.publish_date) - toMili(b.publish_date))
-        .map(feed => <RSSFeedItem item={feed} />)
+
+    let content;
+    if (loading) {
+        content = Array.from({ length: 5 }, (_, i) => <RSSFeedPlaceholder key={i} />)
+    } else {
+        content = feeds
+            .sort((a, b) => toMili(a.publish_date) - toMili(b.publish_date))
+            .map(feed => <RSSFeedItem item={feed} />)
+    }
+
+
     return (
-        <div>
-            <div>
-                {showFilter && <FilterFeeds onFilter={onFilter} />}
-                <button onClick={() => setShowFilter(v => !v)}></button>
+        <div className="py-3">
+            <h2 className="text-center">Latest News</h2>
+            <div className="d-flex flex-row align-items-start justify-content-between mb-2">
+                <div className="pe-2 flex-grow-1">
+                    {showFilter && <FilterFeeds onFilter={(f) => { setShowFilter(false); onFilter(f) }} />}
+                </div>
+                <div className="ps-2">
+                    <button className="btn btn-secondary" onClick={() => setShowFilter(v => !v)}><i className="bi bi-funnel"></i></button>
+                </div>
             </div>
-            <ul>{feedsList}</ul>
+            <div className="mt-2 p-3 border rounded">
+                {content.length !== 0 && <ul className="list-group">{content}</ul>}
+                {content.length === 0 && <div className="text-center">There is no news</div>}
+            </div>
         </div>
     )
 }
