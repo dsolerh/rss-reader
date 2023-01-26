7118547a0cde3026dcb39dc74cfafad18a689125
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-26 19:25:46 +0200

    fix word wapping style issue

--- reader-front/src/components/FeedSourceItem.tsx
+++ reader-front/src/components/FeedSourceItem.tsx
@@ -7,10 +7,12 @@ interface FeedSourceItemProps {
 export const FeedSourceItem = ({ link, index, onDelete }: FeedSourceItemProps) => {
     return (
         <li className="list-group-item" key={index}>
-            <div className="d-flex flex-row justify-content-between">
-                <span>{link}</span>
-                <button className="btn btn-secondary" onClick={() => { onDelete(index) }}><i className="bi bi-trash3"></i></button>
+            <div className="row g-4">
+                <div className="col-10"><span style={{ wordBreak: "break-all" }}>{link}</span></div>
+                <div className="col-2">
+                    <button className="btn btn-secondary" onClick={() => { onDelete(index) }}><i className="bi bi-trash3"></i></button>
+                </div>
             </div>
-        </li>
+        </li >
     )
 }
