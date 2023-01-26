ea380b38ec33892ff74b02c1f79025bd3d20bcad
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-26 12:34:29 +0200

    don't add feed if invalid

--- reader-front/src/components/ControlPanel.tsx
+++ reader-front/src/components/ControlPanel.tsx
@@ -16,6 +16,7 @@ export const ControlPanel = ({ onChangeFeed }: ControlPanelProps) => {
     const [feeds, setFeeds] = useState<string[]>([])
 
     const addFeed = () => {
+        if (feed.error || !feed.touched) { return }
         setFeeds(feeds => [...feeds, feed.value])
     }
 
