0d0557e72fd7e4872a53f01dedc9b05413fa4e36
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-26 16:31:52 +0200

    add styles to the RSS feed item

--- reader-front/src/components/RRSFeedItem.tsx
+++ reader-front/src/components/RRSFeedItem.tsx
@@ -5,13 +5,22 @@ interface RSSFeedItemProps {
 }
 export const RSSFeedItem = ({ item }: RSSFeedItemProps) => {
     return (
-        <li>
-            <a href={item.link} target="_blank">
-                <div>{item.title}</div>
-                <div>{item.source}</div>
-                <div>{item.publish_date}</div>
-                <div>{item.description}</div>
-            </a>
+        <li className="list-group-item">
+            <div className="card">
+                <div className="card-header">
+                    <h5 className="card-title">{item.title}</h5>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">{item.description}</p>
+                    <div className="d-flex justify-content-end">
+                        <a href={item.link} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">Read more</a>
+                    </div>
+                </div>
+                <div className="card-footer d-flex justify-content-between text-muted">
+                    <span>From: {item.source}</span>
+                    <span>Date: {item.publish_date}</span>
+                </div>
+            </div>
         </li>
     )
 }
